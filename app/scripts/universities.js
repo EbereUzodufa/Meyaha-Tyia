bc8003bb82ae9dff61e94fd602a9e6c3511c22d5
Ebere <ebereuzodufa@gmail.com>
2019-09-22 18:43:50 +0100

    confirm school data is not null before populating

--- app/scripts/universities.js
+++ app/scripts/universities.js
@@ -8,23 +8,25 @@ const fetchData = ()=>{
 const build = (resp) =>{
     const schools = resp.Schools;
     // console.log('shools', schools);
-    const article = $('article.schools-article');
-    const div = document.createElement('div');
-    div.classList.add('partner-schools');
-    // const frag = document.createDocumentFragment();
-    let frag = '';
-    const parentImgSchSrc = 'images/partner-universities';
-    schools.forEach(school => {
-        const imgSrc = parentImgSchSrc + '/' + school.image;
-        frag += `
-            <a class="partner-school" href="${school.website}" title = "${school.name}" target="_blank">
-                <img src="${imgSrc}"  alt="Official logo of ${school.name}" class="partner-school__img"/>
-            </a>
-        `
-    });
+    if (schools) {
+        const article = $('article.schools-article');
+        const div = document.createElement('div');
+        div.classList.add('partner-schools');
+        // const frag = document.createDocumentFragment();
+        let frag = '';
+        const parentImgSchSrc = 'images/partner-universities';
+        schools.forEach(school => {
+            const imgSrc = parentImgSchSrc + '/' + school.image;
+            frag += `
+                <a class="partner-school" href="${school.website}" title = "${school.name}" target="_blank">
+                    <img src="${imgSrc}"  alt="Official logo of ${school.name}" class="partner-school__img"/>
+                </a>
+            `
+        });
 
-    div.innerHTML =frag;
-    article.append(div);
+        div.innerHTML =frag;
+        article.append(div);
+    }
 }
 
 const startApp = () =>{
