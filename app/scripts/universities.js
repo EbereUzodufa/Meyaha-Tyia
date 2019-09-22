fe8810b694bd83f5384843bbbec81baa4c7f979e
Ebere <ebereuzodufa@gmail.com>
2019-09-22 18:41:03 +0100

    Adds function to fetch and populate data
    The fetchData function fetches data from university.json. The build is used to populate the data in the DOM.

---
+++ app/scripts/universities.js
@@ -0,0 +1,34 @@
+const fetchData = ()=>{
+    fetch('data/universities.json')
+        .then((resp) => resp.json())
+        .then(build)
+        .catch((err)=>console.log('Error', err));
+}
+
+const build = (resp) =>{
+    const schools = resp.Schools;
+    // console.log('shools', schools);
+    const article = $('article.schools-article');
+    const div = document.createElement('div');
+    div.classList.add('partner-schools');
+    // const frag = document.createDocumentFragment();
+    let frag = '';
+    const parentImgSchSrc = 'images/partner-universities';
+    schools.forEach(school => {
+        const imgSrc = parentImgSchSrc + '/' + school.image;
+        frag += `
+            <a class="partner-school" href="${school.website}" title = "${school.name}" target="_blank">
+                <img src="${imgSrc}"  alt="Official logo of ${school.name}" class="partner-school__img"/>
+            </a>
+        `
+    });
+
+    div.innerHTML =frag;
+    article.append(div);
+}
+
+const startApp = () =>{
+    fetchData();
+}
+
+startApp();
